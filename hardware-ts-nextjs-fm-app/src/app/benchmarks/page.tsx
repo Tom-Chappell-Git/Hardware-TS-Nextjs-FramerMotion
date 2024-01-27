6e69dbbb7dc34d37916ed214f1fcaa162877048b
Thomas Chappell <tom.chappell.info@gmail.com>
2024-01-27 04:45:31 +0000

    added pseudocode for comparison step. tbc

--- hardware-ts-nextjs-fm-app/src/app/benchmarks/page.tsx
+++ hardware-ts-nextjs-fm-app/src/app/benchmarks/page.tsx
@@ -15,6 +15,9 @@ const Benchmarks: React.FC = () => {
 
   // Define the state for the search term and set the initial value to an empty string 
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTerm2, setSearchTerm2] = useState<string>('');
+
+
 
 
   // Render the component
@@ -25,14 +28,12 @@ const Benchmarks: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="grid lg:grid-cols-2">
+      <main className="grid lg:grid-cols-2 gap-4">
 
           <section>
             {/* <Navbar /> */}
           <Navbar />
-          </section>
-
-          <section>
+      
 
           {/* <Search bar /> */}
           <motion.div
@@ -41,7 +42,7 @@ const Benchmarks: React.FC = () => {
             transition={{ duration: 1 }}
           >
             <div className="flex justify-left items-center mt-24">
-              <div className="w-1/2 flex justify-center items-center">
+              <div className="w-full flex justify-center items-center">
                 <input
                   className="text-black font-semibold / border-2 border-white rounded-lg w-full / pl-2 py-1"
                   type="text"
@@ -63,11 +64,11 @@ const Benchmarks: React.FC = () => {
                 <thead>
                   <tr>
                     {benchmarksJson.header.map((headerItem, index) => (
-                      <th className=" pr-4" key={index}>{headerItem}</th>
+                      <th className=" p-2" key={index}>{headerItem}</th>
                     ))}
                   </tr>
                 </thead>
-                <tbody>
+                <tbody className="cursor-pointer">
                   {benchmarksJson.body
                     .filter((item) =>
                       searchTerm === ''
@@ -91,7 +92,59 @@ const Benchmarks: React.FC = () => {
           </section>
 
           <section>
-            
+          {/* <Search bar /> */}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <div className="flex justify-left items-center mt-24">
+              <div className="w-full flex justify-center items-center">
+                <input
+                  className="text-black font-semibold / border-2 border-white rounded-lg w-full / pl-2 py-1"
+                  type="text"
+                  placeholder="Search for a CPU or GPU"
+                  value={searchTerm2}
+                  onChange={(e) => setSearchTerm2(e.target.value)}
+                />
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <div className="mt-20">
+              <table className="border-2 w-full mx-auto">
+                <thead>
+                  <tr>
+                    {benchmarksJson.header.map((headerItem, index) => (
+                      <th className=" p-2" key={index}>{headerItem}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody className="cursor-pointer">
+                  {benchmarksJson.body
+                    .filter((item) =>
+                      searchTerm2 === ''
+                        ? true
+                        : (item as string[]).some((cell) =>
+                            cell && typeof cell === 'string' && cell.toLowerCase().includes(searchTerm2.toLowerCase())
+                          )
+                    )
+                    .map((row, rowIndex) => (
+                      <tr key={rowIndex}>
+                        {row.map((cell, cellIndex) => (
+                          <td className="pt-2 pl-2 border-t-2 border-white/70" key={cellIndex}>{cell}</td>
+                        ))}
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+            </motion.div>
           </section>
 
        </main>
@@ -101,3 +154,16 @@ const Benchmarks: React.FC = () => {
 
 // Export the component
 export default Benchmarks;
+
+
+// Pseudocode for comparison function:
+// make the cell of the table clickable
+// when clicked, the cell will be highlighted
+
+// the cell will be compared to the other cell
+// if the cell is greater than the other cell, the cell will be highlighted green with an arrow pointing up
+// if the cell is less than the other cell, the cell will be highlighted red with an arrow pointing down
+
+// ------------------------------
+
+// 
