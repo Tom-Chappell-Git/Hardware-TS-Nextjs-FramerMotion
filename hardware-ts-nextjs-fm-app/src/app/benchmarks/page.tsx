7d0826b8d1015536c1c965ca7bdc6273639f775b
Thomas Chappell <tom.chappell.info@gmail.com>
2024-02-03 01:01:15 +0000

    comparison update bug fixed. comments needed

--- hardware-ts-nextjs-fm-app/src/app/benchmarks/page.tsx
+++ hardware-ts-nextjs-fm-app/src/app/benchmarks/page.tsx
@@ -3,7 +3,7 @@
 // Import React and any other necessary dependencies
 import { motion } from "framer-motion"
 import Head from "next/head"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import Navbar from "../components/navbar";
 import benchmarksJson from '../productinfos/blender.json';
@@ -18,6 +18,26 @@ const Benchmarks: React.FC = () => {
   const [highlightedRow, setHighlightedRow] = useState<number | null>(null);
   const [highlightedRow2, setHighlightedRow2] = useState<number | null>(null);
 
+  
+
+  // Define the filtered data for table 1
+  const updatedTable = benchmarksJson.body.filter((item) =>
+    searchTerm === ''
+      ? true
+      : (item as string[]).some((cell) =>
+          cell && typeof cell === 'string' && cell.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+  );
+
+  // Define the filtered data for table 2
+  const updatedTable2 = benchmarksJson.body.filter((item) =>
+    searchTerm2 === ''
+      ? true
+      : (item as string[]).some((cell) =>
+          cell && typeof cell === 'string' && cell.toLowerCase().includes(searchTerm2.toLowerCase())
+        )
+  );
+
   // Render the component
   return (
     <>
@@ -30,7 +50,7 @@ const Benchmarks: React.FC = () => {
 
         <section className="mr-4">
           <Navbar />
-
+          
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -49,16 +69,48 @@ const Benchmarks: React.FC = () => {
             </div>
           </motion.div>
 
+
+{/* Display value of Score cell after updating after a search (table 1) */}
+
+          {highlightedRow !== null && (
+            <div className="mt-4">
+              <h2 className="underline">Selected Row:</h2>
+              <p>{benchmarksJson.body.filter((item) =>
+                searchTerm === ''
+                  ? true
+                  : (item as string[]).some((cell) =>
+                    cell && typeof cell === 'string' && cell.toLowerCase().includes(searchTerm.toLowerCase())
+                  )
+              )[highlightedRow][1]}</p>
+            </div>
+          )}
+{/* {/* Display value of Score cell after updating after a search (table 2) */}
+
+          {highlightedRow2 !== null && (
+            <div className="mt-4">
+              <h2 className="underline">Selected Row:</h2>
+              <p>{benchmarksJson.body.filter((item) =>
+                searchTerm2 === ''
+                  ? true
+                  : (item as string[]).some((cell) =>
+                    cell && typeof cell === 'string' && cell.toLowerCase().includes(searchTerm2.toLowerCase())
+                  )
+              )[highlightedRow2][1]}</p>
+            </div>
+          )}
+          
+
+
           {highlightedRow !== null && highlightedRow2 !== null && (() => {
-            const percentageIncrease = ((Number(benchmarksJson.body[highlightedRow][1]) - Number(benchmarksJson.body[highlightedRow2][1])) / Number(benchmarksJson.body[highlightedRow2][1])) * 100;
+            const percentageIncrease = ((Number(updatedTable[highlightedRow][1]) - Number(updatedTable2[highlightedRow2][1])) / Number(updatedTable2[highlightedRow2][1])) * 100;
             return (
               <div className="mt-4">
                 <h2 className="underline">Comparison:</h2>
 
                 {percentageIncrease > 0 ? (
-                  <p style={{ color: '#42f542' }}>{benchmarksJson.body[highlightedRow][0]} has a {percentageIncrease.toFixed(2)}% higher Median Score</p>
+                  <p style={{ color: '#42f542' }}>{updatedTable[highlightedRow][0]} has a {percentageIncrease.toFixed(2)}% higher Median Score</p>
                 ) : (
-                  <p style={{ color: '#ff0011' }}>{benchmarksJson.body[highlightedRow][0]} has a {Math.abs(percentageIncrease).toFixed(2)}% lower Median Score</p>
+                  <p style={{ color: '#ff0011' }}>{updatedTable[highlightedRow][0]} has a {Math.abs(percentageIncrease).toFixed(2)}% lower Median Score</p>
                 )}
               </div>
             );
@@ -69,7 +121,7 @@ const Benchmarks: React.FC = () => {
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
           >
-            <div className="mt-20">
+            <div className="mt-10">
               <table className="border-2 w-full mx-auto">
                 <thead>
                   <tr>
@@ -114,6 +166,8 @@ const Benchmarks: React.FC = () => {
           </motion.div>
         </section>
 
+
+{/* ------------------- Table 2 ----------------- */}
         <section>
           <motion.div
             initial={{ opacity: 0 }}
@@ -134,15 +188,15 @@ const Benchmarks: React.FC = () => {
           </motion.div>
 
           {highlightedRow !== null && highlightedRow2 !== null && (() => {
-            const percentageIncrease = ((Number(benchmarksJson.body[highlightedRow][1]) - Number(benchmarksJson.body[highlightedRow2][1])) / Number(benchmarksJson.body[highlightedRow2][1])) * 100;
+            const percentageIncrease = ((Number(updatedTable2[highlightedRow][1]) - Number(updatedTable2[highlightedRow2][1])) / Math.abs(Number(updatedTable2[highlightedRow2][1]))) * 100;
             return (
               <div className="mt-4">
                 <h2 className="underline">Comparison:</h2>
 
                 {percentageIncrease > 0 ? (
-                  <p style={{ color: '#ff0011' }}>{benchmarksJson.body[highlightedRow2][0]} has a {percentageIncrease.toFixed(2)}% lower Median Score</p>
+                  <p style={{ color: '#ff0011' }}>{updatedTable2[highlightedRow2][0]} has a {percentageIncrease.toFixed(2)}% lower Median Score</p>
                 ) : (
-                  <p style={{ color: '#42f542' }}>{benchmarksJson.body[highlightedRow2][0]} has a {Math.abs(percentageIncrease).toFixed(2)}% higher Median Score</p>
+                  <p style={{ color: '#42f542' }}>{updatedTable2[highlightedRow2][0]} has a {Math.abs(percentageIncrease).toFixed(2)}% higher Median Score</p>
                 )}
               </div>
             );
@@ -153,7 +207,7 @@ const Benchmarks: React.FC = () => {
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
           >
-            <div className="mt-20">
+            <div className="mt-10">
               <table className="border-2 w-full mx-auto">
                 <thead>
                   <tr>
