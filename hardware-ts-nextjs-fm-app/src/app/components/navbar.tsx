6a9daf69f42e0300318bec338980c2e9d286440c
Thomas Chappell <tom.chappell.info@gmail.com>
2024-02-06 15:56:17 +0000

    added prototype gaming benchmarks page and navbar update

--- hardware-ts-nextjs-fm-app/src/app/components/navbar.tsx
+++ hardware-ts-nextjs-fm-app/src/app/components/navbar.tsx
@@ -11,9 +11,26 @@ const Navbar: React.FC = () => {
                 transition={{ duration: 1 }}
             >
                 <Link href="/">Home</Link>
-                <Link href="/cpus">CPUs</Link>
-                <Link href="/gpus">GPUs</Link>
-                <Link href="/benchmarks">Benchmarks</Link>
+
+                <div className="relative group">
+                    <Link href="/benchmarks">Hardware</Link>
+                    <div className="absolute left-0  hidden group-hover:block bg-black text-white shadow-md rounded-md">
+                        <ul className="mx-6">
+                            <li><Link href="/cpus" className="">CPUs</Link></li>
+                            <li><Link href="/gpus" className="">GPUs</Link></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="relative group ">
+                    <Link href="/benchmarks">Benchmarks</Link>
+                    <div className="absolute left-0 hidden group-hover:block bg-black text-white shadow-md rounded-md">
+                        <ul className="px-6">
+                            <li><Link href="/benchmarks" className="">Blender</Link></li>
+                            <li><Link href="/gaming" className="">Gaming</Link></li>
+                        </ul>
+                    </div>
+                </div>
             </motion.nav>
         </>
     );
