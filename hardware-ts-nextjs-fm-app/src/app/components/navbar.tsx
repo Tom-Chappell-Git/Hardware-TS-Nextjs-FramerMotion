7db6268ad833a7bf11d3f79a3087730e7b856b91
Thomas Chappell <tom.chappell.info@gmail.com>
2024-02-15 15:02:27 +0000

    push before library test

--- hardware-ts-nextjs-fm-app/src/app/components/navbar.tsx
+++ hardware-ts-nextjs-fm-app/src/app/components/navbar.tsx
@@ -10,32 +10,32 @@ const Navbar: React.FC = () => {
                 exit={{ opacity: 0, height: 0 }}
                 transition={{ duration: 1 }}
             >
-                <div className="relative group">
-                    <Link href="/benchmarks">Info</Link>
-                    <div className="absolute left-0  hidden group-hover:block bg-black text-white shadow-md rounded-md">
-                        <ul className="mx-6">
-                            <li><Link href="/" className="">Home</Link></li>
-                            <li><Link href="/cpubreakdown" className="">Cpu Breakdown</Link></li>
+                <div className="relative group w-full">
+                    <Link href="/benchmarks" className="hover:text-cyan-200">Info</Link>
+                    <div className="absolute left-0  hidden group-hover:block bg-black text-white shadow-md border-white border-2 rounded-md">
+                        <ul className="mx-6 ">
+                            <li><Link href="/" className="hover:text-cyan-200">Home</Link></li>
+                            <li><Link href="/cpubreakdown" className="hover:text-cyan-200">Cpu Breakdown</Link></li>
                         </ul>
                     </div>
                 </div>
 
-                <div className="relative group">
-                    <Link href="/benchmarks">Hardware</Link>
-                    <div className="absolute left-0  hidden group-hover:block bg-black text-white shadow-md rounded-md">
+                <div className="relative group w-full">
+                    <Link href="/benchmarks" className="hover:text-cyan-200">Hardware</Link>
+                    <div className="absolute left-0  hidden group-hover:block bg-black text-white shadow-md border-white border-2 rounded-md">
                         <ul className="mx-6">
-                            <li><Link href="/cpus" className="">CPUs</Link></li>
-                            <li><Link href="/gpus" className="">GPUs</Link></li>
+                            <li><Link href="/cpus" className="hover:text-cyan-200">CPUs</Link></li>
+                            <li><Link href="/gpus" className="hover:text-cyan-200">GPUs</Link></li>
                         </ul>
                     </div>
                 </div>
 
                 <div className="relative group ">
-                    <Link href="/benchmarks">Benchmarks</Link>
-                    <div className="absolute left-0 hidden group-hover:block bg-black text-white shadow-md rounded-md">
+                    <Link href="/benchmarks" className="hover:text-cyan-200">Benchmarks</Link>
+                    <div className="absolute left-0 hidden group-hover:block bg-black text-white shadow-md border-white border-2 rounded-md">
                         <ul className="px-6">
-                            <li><Link href="/benchmarks" className="">Blender</Link></li>
-                            <li><Link href="/gaming" className="">Gaming</Link></li>
+                            <li><Link href="/benchmarks" className="hover:text-cyan-200">Blender</Link></li>
+                            <li><Link href="/gaming" className="hover:text-cyan-200">Gaming</Link></li>
                         </ul>
                     </div>
                 </div>
